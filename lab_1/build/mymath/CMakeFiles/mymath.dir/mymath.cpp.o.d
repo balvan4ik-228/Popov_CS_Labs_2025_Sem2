722fceb4b10e11f80ed55e0e691871ab86e8ac00
balvan4ik-228 <home-andru@mail.ru>
2025-04-22 14:30:28 +0300

    Delete lab_1 directory

--- lab_1/build/mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
+++
@@ -1,4 +0,0 @@
-mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
- /home/klownada/Popov_CS_Labs_2025_Sem2/mymath/mymath.cpp \
- /usr/include/stdc-predef.h \
- /home/klownada/Popov_CS_Labs_2025_Sem2/mymath/mymath.h
--- lab_1/mymath/mymath.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYMATH_H
-#define MYMATH_H
-
-namespace mymath {
-    int sum(int a, int b);
-    int times(int a, int b);
-}
-
-#endif //MYMATH_H
